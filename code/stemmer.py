1da86e45f4ceb91838b24641795f137c588b8b81
Sebastian <basti@slayer>
2022-02-12 15:26:40 +0100

    Stemmer now working

--- code/stemmer.py
+++ code/stemmer.py
@@ -1,12 +1,53 @@
 import os 
+import pandas as pd
+import sentencepiece as spm
+import ctranslate2
+import re
+import multiprocessing
+from utils.stemming import *
 
-
-
-    
-def stem_file(path):
+def stem_file(data):
+    path,lang = data
+    print("NOW PROCESSING",path)
     cfile = open(path,'r')
-    for line in cfile:
+    path_short = os.path.splitext(path)[0]
+    segmentnrs, lines, cleaned_lines = text2list(path,lang)
+    text_df = pd.DataFrame({'segmentnrs': segmentnrs, 'original': lines, "stemmed": cleaned_lines})
+    if lang == "skt":
+        chunk_lists = text_df['stemmed'].apply(lambda line: chunk_line(line), 120)
+        chunk_exploded = chunk_lists.explode()
+        chunk_list = chunk_exploded.tolist()
+        tokenizer = \
+            spm.SentencePieceProcessor(model_file = location + "spm/skt-tag8k.model")
+        tonenized_chunk_list = tokenizer.encode(chunk_list, out_type=str)
+        translator = ctranslate2.Translator(location + "model/",
+                                        intra_threads=4,
+                                        device='cpu')
+        transres_exploded = \
+            pd.Series(translator.translate_batch(tonenized_chunk_list,
+                max_batch_size=548),
+                index = chunk_exploded.index)
+        
+        # pd.Series.apply: https://towardsdatascience.com/avoiding-apply-ing-yourself-in-pandas-a6ade4569b7f
+        transres_exploded = transres_exploded.apply(lambda line:
+            tokenizer.decode(line[0]['tokens']) )
+
+        # leave only the stems
+        transres_exploded = transres_exploded.apply(transres2stemlist)
+        text_df['stemmed'] = \
+            transres_exploded.groupby(chunk_exploded.index).apply(lambda lists:
+        list(itertools.chain(*lists)) )
+    text_df.to_csv(path_short + ".tsv", sep='\t')
 
 def run_stemmer(path,lang,num_of_threads):
-    for 
-    print(path,lang)     
+    list_of_paths = []
+    for cfile in os.listdir(path):
+        filename = os.fsdecode(cfile)
+        # make sure we only read txt-files
+        if ".txt" in filename:
+            list_of_paths.append([path+filename,lang])
+    pool = multiprocessing.Pool(processes=num_of_threads)
+    quote_results = pool.map(stem_file, list_of_paths)
+    pool.close()
+
+run_stemmer("../test/","tib",1)    
---
+++ code/utils/stemming.py
@@ -0,0 +1,142 @@
+from xliterator import unicode_to_internal_transliteration
+import re
+
+TIBETAN_STEMFILE="data/verbinator_tabfile.txt"
+
+
+
+def initialize_tibetan_stemmer(tibetan_stemfile):
+    global r
+    r = open(tibetan_stemfile,'r')
+    stems = {}
+    
+    for line in r:
+        headword = line.split('\t')[0]
+        entry = line.split('\t')[2].strip()
+        stems[headword] = entry
+    return stems 
+
+tib_stems = initialize_tibetan_stemmer(TIBETAN_STEMFILE)
+
+def multireplace(string,tib_stems):
+    string1 = string
+    if string in tib_stems:
+        string = tib_stems[string]
+    return string
+
+
+
+def prepare_skt(string):
+    string = string.lower()
+    string = re.sub(r"// ?([^ ]+_[^ ]+) ?//","",string)
+    string = re.sub(r"\|\| ?([^ ]+_[^ ]+) ?\|\|","",string)
+    string = re.sub(r'[^\s]+[0-9][^\s]+',"",string)
+    string = string.replace('ñ ','ṃ ')
+    string = re.sub(r"[^a-zA-ZāĀīĪūŪṛṚṝḷḶḹṅṄñÑṭṬḍḌṇṆśŚṣṢṃḥēōḻṟṉḵṯ \n]","",string)
+    string = string.strip()
+    string = unicode_to_internal_transliteration(string)
+    return string
+
+def prepare_tib(string):    
+    result = ''
+    string = string.replace("+"," ")
+    for word in string.split():
+        if not "/" in word and not "@" in word and re.search("[a-zA-Z]",word):
+            word_stripped = word.split("'")[0]
+            result += multireplace(word_stripped,tib_stems) + " "
+    return result
+
+def prepare(string,lang):
+    if lang == "skt":
+        string = prepare_skt(string)
+    elif lang == "tib":
+        string = prepare_tib(string)
+    return string
+        
+
+def chunk_line(line, maxlen):
+    line_chunks = []
+
+    chunk = ""
+    tokens = line.split(' ')
+    last_index = len(tokens) - 1
+    for index, token in enumerate(tokens):
+        chunk += token + " "
+        if index == last_index or len(chunk) > maxlen:
+            line_chunks.append(chunk)
+            chunk = ""
+    if chunk:
+        line_chunks.append(chunk)
+    return line_chunks
+
+def transres2stemlist(transres):
+    stemlist = []
+    for result in transres.split('#'):
+        result = result.strip()
+        if result == '': break
+        stem = result.split(' ')[0]
+        stemlist.append(stem)
+    return stemlist
+
+def create_segnr(folio,count,filepath):
+    # include folio numbers for KG/TG, not for other files
+    filename = re.sub(".*/","",filepath)
+    filename = re.sub("\..*","",filename)
+    filename = filename.replace(":","-") 
+    if "NK" in filename or "NG" in filename or len(folio) == 0:
+        return filename + ":" + str(count)
+    else:
+        return filename + ":" + folio + '-' + str(count)\
+
+
+def get_folio_number(line):
+    folio_number = ""
+    m =  re.search("@([0-9]+[abAB])",line)
+    if m:
+        folio_number = m.group(1)
+        return folio_number
+    # this is for NyGB
+    else:
+        m =  re.search("([0-9])+[^a-zA-Z@]+@",line)
+        if m:
+            folio_number = m.group(1)
+            return folio_number.lower()
+
+def text2list(text_path,lang):
+    lines = []
+    cleaned_lines = []
+    segmentnrs = []
+    folio = ""
+    count = 0 
+    with open(text_path,"r") as text:
+        prefix = ""
+        for line in text:
+            # For Tibetan, we extract the folio numbers from the lines
+            if lang == "tib":
+                new_folio = get_folio_number(line)
+                if new_folio and not"NK" in text_path and not "NG" in text_path:
+                    folio = new_folio
+                    count = 0
+                    
+            line = prefix + line  #.strip()
+            if not re.search(r"[a-zA-Z]", line):
+                prefix += line + " "
+            else:
+                prefix = ""
+                line = line.strip()
+                # a bit of tibetan-specific preprocessing
+                if lang == "tib":
+                    line = line.lower()
+                    if "T" in text_path:
+                        line = line.replace("ts","tsh")
+                        line = line.replace("tz","ts")
+                    line = line.replace(',','/')
+                    line = line.replace('|','/')
+                segmentnr = create_segnr(folio,count,text_path)
+                cleaned_line = prepare(line,lang)
+                lines.append(line)
+                cleaned_lines.append(cleaned_line)
+                segmentnrs.append(segmentnr)
+                count += 1
+    return [segmentnrs,lines,cleaned_lines]
+        
