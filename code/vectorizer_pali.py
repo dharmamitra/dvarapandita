8f49a2f2c61de99d37c1138bed9670cbd01523ac
Vladimir Angirov <angirov@posteo.net>
2024-01-12 14:06:51 +0100

    vectorizer pali almost done

--- code/vectorizer_pali.py
+++ code/vectorizer_pali.py
@@ -1,5 +1,4 @@
 import re
-from random import randint
 from pathlib import Path
 
 import pandas as pd
@@ -7,7 +6,8 @@ import numpy as np
 import fasttext
 
 from utils.constants import * ###################################################
-from file_mngr import FileMngr
+from file_mngr import FileMngr, TextFile
+
 
 class Vectorizer:
     """tsv(segmentId, orig-text) --> tsv(segmentId, orig-text, tokenized-text)
@@ -16,8 +16,8 @@ class Vectorizer:
     def __init__(self, 
                  lang: str, 
                  input_dir: str, 
-                 n_buckets,
-                 threads,
+                 threads=1,
+                 n_buckets=1,
                  resume_mode=True, 
                  input_extention=".tsv",
                  sep="\t",
@@ -27,53 +27,52 @@ class Vectorizer:
         self.lang: str = lang
         self.resume_mode = resume_mode
         self.sep = sep
-        self.file_mngr = FileMngr(input_dir, 
+        self.file_mngr = FileMngr(n_buckets=n_buckets,
+                                  input_dir = input_dir, 
                                   input_extention = input_extention,
                                   output_dir = output_dir,
                                   output_extention = output_extention
                                   )
-        
+
         ################## Lang specific ########################  --> LanguageMngr ???
         self.windowsize = WINDOWSIZE[lang]
+        self.splitter = self.init_splitter()
         # self.exploder = self.init_exploder
-        # self.splitter = self.init_splitter()
         # self.tokenizer = self.init_tokenizer()
-        self.cleaner = self.init_cleaner()
-        self.vector_model = self.get_vector_model()
+        # self.cleaner = self.init_cleaner()
+        self.vector_model = self.init_vector_model() #fasttext
         self.stopwords = self.read_stopwords()
 
     def create_vectorfile(self, file_path: Path):
-        print("NOW PROCESSING",file_path.stem)
+        text = TextFile(self.lang, file_path)
+        print("NOW PROCESSING", text.name)
 
-        filename = file_path.stem
-        file_df = pd.read_csv(file_path, 
+        text.segments_df = pd.read_csv(file_path, 
                               sep=self.sep, 
                               names=['segmentId', 'original_text', 'stemmed_text'],
-                              on_bad_lines="skip").astype(str)
-#################################################################################
-########################     continue here         ##############################
-#################################################################################
-        vec_df = self.exploder(file_df, self.lang)
-        bucket_number = randint(1, self.n_buckets)
-        bucket_path = out_path + "folder" + str(bucket_number)
+                              on_bad_lines="skip")
+        text.words_df = self.explode_segment_df(text.segments_df)
+        text.words_df = self.window_vec_exploded_df(text.words_df) 
+        return text
 
-        if not os.path.isdir(bucket_path):
-            os.mkdir(bucket_path)
-        vec_df.to_pickle(bucket_path + "/" + filename + ".p")
-        
+    def explode_segment_df(self, segments_df):
+        # 1. the segments are splitted into word lists
+        words_df = segments_df.drop(columns="original_text")
+        words_df['stemmed_text'] = words_df['stemmed_text'].apply(self.splitter)
+        # 2. the table is "streched" by the word lists
+        return words_df.explode("stemmed_text")
 
-    def exploder(self, file_df):
-        file_df['stemmed_text'] = file_df['stemmed_text'].apply(self.split)
-        vec_df = file_df.explode("stemmed_text")
-        vec_df['weights'] = vec_df['stemmed_text'].apply(lambda word: self.get_weight(word, self.lang))
-        vec_df['vectors'] = vec_df['stemmed_text'].apply(lambda word: self.get_vector(word, self.vector_model))
-        vector_list = vec_df['vectors'].tolist()
-        weight_list = vec_df['weights'].tolist()
-        vec_df['sumvectors'] = self.get_sumvectors(vector_list, weight_list, self.windowsize)
-        return vec_df
+    def window_vec_exploded_df(self, file_df):
+        file_df['weights'] = file_df['stemmed_text'].apply(lambda word: self.get_weight(word))
+        file_df['vectors'] = file_df['stemmed_text'].apply(lambda word: self.get_vector(word))
+        file_df['sumvectors'] = self.calc_win_vecs(
+            file_df['vectors'].tolist(), 
+            file_df['weights'].tolist()
+        )
+        return file_df
 
-    def get_vector_model(lang):
-        return fasttext.load_model(VECTOR_PATH + lang + ".bin")
+    def init_vector_model(self):
+        return fasttext.load_model(VECTOR_PATH + self.lang + ".bin")
 
     def read_stopwords(self):
         f = open(STOPWORDS_PATH + self.lang + "_stop.txt", 'r')
@@ -92,8 +91,12 @@ class Vectorizer:
             else:        
                 return 1
 
-    def get_vector(stem,vector_model):
-        return vector_model.get_word_vector(stem)
+    def get_vector(self, word):
+        try:
+            assert type(word) == str
+            return self.vector_model.get_word_vector(word)
+        except:
+            print(f"{word}: type {type(word)}")
 
     def get_sumvector(self, vectors,weights=False):
         if weights:
@@ -108,9 +111,25 @@ class Vectorizer:
                 pool.append(np.average(vectors[0:i],axis=0,weights=weigths[0:i]))
         return np.mean(pool,axis=0)
 
-    def get_sumvectors(self, vector_list, weight_list):
+    def calc_win_vecs(self, vector_list, weight_list):
         sumvectors = []
         for i in range(len(vector_list)):
             k = i + self.windowsize
             sumvectors.append(self.get_sumvector(vector_list[i:k], weight_list[i:k]))
         return sumvectors
+
+    ########## Language specific functions ##########
+    def init_splitter(self):
+        match self.lang:
+            case "skt":
+                return split_sanskrit_stem
+            case other:
+                return str.split
+
+    def split_sanskrit_stem(stems):
+        result = []
+        for stem in stems.split("#"):
+            stem = stem.strip().split(" ")[0]
+            if len(stem) > 0:
+                result.append(stem)
+        return result
